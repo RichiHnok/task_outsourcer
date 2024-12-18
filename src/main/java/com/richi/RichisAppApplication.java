f5a56f0b42fbbeb68f374dc8d6f42f3c07a2c06d
Artemiy Viazovski <grenkavoit@gmail.com>
2024-12-18 08:50:01 +0300

    Добавил возможность делать миграции бд

--- src/main/java/com/richi/RichisAppApplication.java
+++ src/main/java/com/richi/RichisAppApplication.java
@@ -1,4 +1,4 @@
-package com.richi.web_part;
+package com.richi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/resources/db/migration/V002__test_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO `my_db`.`users` (`login`, `name`, `surname`, `password`, `email`, `priority`, `minutes_to_proc_task`)
+VALUES ('C', 'C', 'C', '$2a$05$Jy9ysQVWGuwfplxJ9NUfb.S6bjysR7m/19YI6A7UOMONcnei8HPSq', 'a@a.c', '5', '60');
